0f36359e79634417abd1f946066d091fbc40f8b1
Nucion <nicolaslesz@hotmail.com>
2022-06-12 22:53:23 -0300

    mini change to installer

--- installer.py
+++ installer.py
@@ -130,7 +130,7 @@ def setPATH(plinstall_Tath):
 
 
 def netMHCpanInstall(path):
-    install_T_Shell()
+    #install_T_Shell()
     try:
         netMHCpanExtract(path)
     except:
@@ -141,4 +141,4 @@ def netMHCpanInstall(path):
     netMHCpanTest(path)
     setPATH(path)
 
-    santo_grial ="../netMHCpan -p test.txt -BA -a HLA-A01:01 > final_out.txt"
+    
